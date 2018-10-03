661e49528d18bf5200003d67ca535a6474f7d58c
Samarth Dua <samarthdua@gmail.com>
2018-10-03 14:43:35 -0400

    added require Team.php in common

--- app/common.php
+++ app/common.php
@@ -2,6 +2,7 @@
 // Change the working directory to this file.
 chdir(__DIR__);
 set_include_path (__DIR__);
-require 'environment.default.php';
+require 'environment.php';
 /** MODELS **/
 require 'models/Work.php';
+require 'models/Team.php';
