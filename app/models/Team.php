b5c18a878cf6b9a3762163c9ae08dfd265dbf3fa
Samarth Dua <samarthdua@gmail.com>
2018-10-03 14:42:37 -0400

    update team.php

--- app/models/Team.php
+++ app/models/Team.php
@@ -1,32 +1,28 @@
 <?php
-class Work
+class Team
 {
   public $id;
   public $name;
-
+  public $hourly_rate;
   public function __construct($data) {
     $this->id = intval($data['id']);
-    
+    $this->name = $data['name'];
+    $this->hourly_rate = floatval($data['hourly_rate']);
   }
-
-  public static function getWorkByTaskId(int $taskId) {
+  public static function fetchAll() {
     // 1. Connect to the database
     $db = new PDO(DB_SERVER, DB_USER, DB_PW);
     // 2. Prepare the query
-    $sql = 'SELECT * FROM Team';
+    $sql = 'SELECT * FROM Teams';
     $statement = $db->prepare($sql);
     // 3. Run the query
-    $success = $statement->execute(
-
-    );
+    $success = $statement->execute();
     // 4. Handle the results
     $arr = [];
     while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
-      // 4.a. For each row, make a new work object
       $theTeam =  new Team($row);
-      array_push($arr, $workItem);
+      array_push($arr, $theTeam);
     }
-    // 4.b. return the array of work objects
     return $arr;
   }
 }
