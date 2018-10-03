9e0fbebb5941d9e600267bf57ff1625f47234a22
Samarth Dua <samarthdua@gmail.com>
2018-10-03 14:40:15 -0400

    added environment.php

--- app/common.php
+++ app/common.php
@@ -2,6 +2,6 @@
 // Change the working directory to this file.
 chdir(__DIR__);
 set_include_path (__DIR__);
-require 'environment.php';
+require 'environment.default.php';
 /** MODELS **/
 require 'models/Work.php';
