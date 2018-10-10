ff84fc937d06ec85e15bfbf205eb042bbefd82f3
Samarth Dua <samarthdua@gmail.com>
2018-10-10 08:51:26 -0400

    Work Hours Report api pt1

---
+++ app/models/WorkHoursReport.php
@@ -0,0 +1,26 @@
+<?php
+
+class WorkHoursReport
+{
+    public static function fetchByProjectId($projectId) {
+      $db = new PDO(DB_SERVER, DB_USER, DB_PW);
+
+      $sql = 'SELECT DATE(start_date) as date, SUM(hours) AS hours
+              FROM Work, Tasks
+              WHERE Work.task_id = Tasks.id
+              AND Tasks.project_id = ?
+              GROUP BY DATE(start_date)';
+
+      $statement = $db->prepare($sql);
+
+      $success = $statement->execute(
+          [$projectId]
+        );
+
+      $arr = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+      return $arr;
+    }
+
+    
+}
